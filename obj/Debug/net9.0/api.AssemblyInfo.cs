dd28ec768063d7836a80b83bfb6aa964b8c07374
Mc Joseph Agbanlog <Mcjoseph.Agbanlog@wizard-ai.com>
2025-01-06 14:14:09 +0800

    Add client environment configuration, utility functions, and initial HTML structure for settings and dashboard

--- obj/Debug/net9.0/api.AssemblyInfo.cs
+++ obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89e1c7b9873ec71b90ceccd58337df945dd8ddf9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3d488e0add646deb3ac7336a7a825ea206b61f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
