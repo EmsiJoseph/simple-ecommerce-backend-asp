dc188fada4a0bb55f816756b44a97b67c0c7d19e
Mc Joseph Agbanlog <Mcjoseph.Agbanlog@wizard-ai.com>
2025-01-06 15:06:48 +0800

    Remove unused client files and configurations

--- client/scripts/utils.js
+++
@@ -1,6 +0,0 @@
-const TextUtils = {
-    truncateText: (text, maxLength = 50) => {
-        if (!text) return 'N/A';
-        return text.length > maxLength ? text.substring(0, maxLength) + '...' : text;
-    }
-};
--- obj/Debug/net9.0/api.AssemblyInfo.cs
+++ obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26cc14434456c8f75a73e37d5688d3c4349047ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fea7851b1285962d574e91f818b39164efb53132")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
